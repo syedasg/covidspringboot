598b13b0601dc249d4409939b455f15217cb002b
Agha hussain <Agha hussain@DESKTOP-36KRLST>
2020-04-29 21:05:29 -0500

    3rd commit

--- coviddemo/src/main/java/com/covidtracker/coviddemo/model/Countries.java
+++ coviddemo/src/main/java/com/covidtracker/coviddemo/model/Countries.java
@@ -2,6 +2,8 @@ package com.covidtracker.coviddemo.model;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Countries {
 
 
@@ -16,63 +18,83 @@ public class Countries {
                 int NewRecovered;
                 int TotalRecovered;
                 String date;
+                @JsonProperty("Country")
 				public String getCountry() {
 					return Country;
 				}
+                @JsonProperty("Country")
 				public void setCountry(String country) {
 					Country = country;
 				}
+                @JsonProperty("CountryCode")
 				public String getCountryCode() {
 					return CountryCode;
 				}
+                @JsonProperty("CountryCode")
 				public void setCountryCode(String countryCode) {
 					CountryCode = countryCode;
 				}
+                @JsonProperty("Slug")
 				public String getSlug() {
 					return Slug;
 				}
+                @JsonProperty("Slug")
 				public void setSlug(String slug) {
 					Slug = slug;
 				}
+                @JsonProperty("NewConfirmed")
 				public int getNewConfirmed() {
 					return NewConfirmed;
 				}
+                @JsonProperty("NewConfirmed")
 				public void setNewConfirmed(int newConfirmed) {
 					NewConfirmed = newConfirmed;
 				}
+                @JsonProperty("TotalConfirmed")
 				public int getTotalConfirmed() {
 					return TotalConfirmed;
 				}
+                @JsonProperty("TotalConfirmed")
 				public void setTotalConfirmed(int totalConfirmed) {
 					TotalConfirmed = totalConfirmed;
 				}
+                @JsonProperty("NewDeaths")
 				public int getNewDeaths() {
 					return NewDeaths;
 				}
+                @JsonProperty("NewDeaths")
 				public void setNewDeaths(int newDeaths) {
 					NewDeaths = newDeaths;
 				}
+                @JsonProperty("TotalDeaths")
 				public int getTotalDeaths() {
 					return TotalDeaths;
 				}
+                @JsonProperty("TotalDeaths")
 				public void setTotalDeaths(int totalDeaths) {
 					TotalDeaths = totalDeaths;
 				}
+                @JsonProperty("NewRecovered")
 				public int getNewRecovered() {
 					return NewRecovered;
 				}
+                @JsonProperty("NewRecovered")
 				public void setNewRecovered(int newRecovered) {
 					NewRecovered = newRecovered;
 				}
+                @JsonProperty("TotalRecovered")
 				public int getTotalRecovered() {
 					return TotalRecovered;
 				}
+                @JsonProperty("TotalRecovered")
 				public void setTotalRecovered(int totalRecovered) {
 					TotalRecovered = totalRecovered;
 				}
+                @JsonProperty("Date")
 				public String getDate() {
 					return date;
 				}
+                @JsonProperty("Date")
 				public void setDate(String date) {
 					this.date = date;
 				}
--- coviddemo/src/main/java/com/covidtracker/coviddemo/model/Global.java
+++ coviddemo/src/main/java/com/covidtracker/coviddemo/model/Global.java
@@ -1,5 +1,7 @@
 package com.covidtracker.coviddemo.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Global {
 	int NewConfirmed;
 	int  TotalConfirmed;
@@ -7,39 +9,51 @@ public class Global {
 	int  TotalDeaths;
 	int  NewRecovered;
 	int  TotalRecovered;
+	@JsonProperty("NewConfirmed")
 	public int getNewConfirmed() {
 		return NewConfirmed;
 	}
+	@JsonProperty("NewConfirmed")
 	public void setNewConfirmed(int newConfirmed) {
 		NewConfirmed = newConfirmed;
 	}
+	@JsonProperty("TotalConfirmed")
 	public int getTotalConfirmed() {
 		return TotalConfirmed;
 	}
+	@JsonProperty("TotalConfirmed")
 	public void setTotalConfirmed(int totalConfirmed) {
 		TotalConfirmed = totalConfirmed;
 	}
+	@JsonProperty("NewDeaths")
 	public int getNewDeaths() {
 		return NewDeaths;
 	}
+	@JsonProperty("NewDeaths")
 	public void setNewDeaths(int newDeaths) {
 		NewDeaths = newDeaths;
 	}
+	@JsonProperty("TotalDeaths")
 	public int getTotalDeaths() {
 		return TotalDeaths;
 	}
+	@JsonProperty("TotalDeaths")
 	public void setTotalDeaths(int totalDeaths) {
 		TotalDeaths = totalDeaths;
 	}
+	@JsonProperty("NewRecovered")
 	public int getNewRecovered() {
 		return NewRecovered;
 	}
+	@JsonProperty("NewRecovered")
 	public void setNewRecovered(int newRecovered) {
 		NewRecovered = newRecovered;
 	}
+	@JsonProperty("TotalRecovered")
 	public int getTotalRecovered() {
 		return TotalRecovered;
 	}
+	@JsonProperty("TotalRecovered")
 	public void setTotalRecovered(int totalRecovered) {
 		TotalRecovered = totalRecovered;
 	}
